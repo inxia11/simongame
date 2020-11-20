edf53c5ac59a7cce44eea5bbe51bd0b3ad6059f2
inxia11 <dmit.insait@gmail.com>
2020-11-20 21:01:48 +1000

    deployed

---
+++ precache-manifest.60f1cf95d14528aa85611b7060150d28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c744071e3d96a6c50851",
+    "url": "/simongame/css/app.b7e57881.css"
+  },
+  {
+    "revision": "d8bfbf2932d0aef37b9cc00ffd4801dd",
+    "url": "/simongame/index.html"
+  },
+  {
+    "revision": "c744071e3d96a6c50851",
+    "url": "/simongame/js/app.3b87bb34.js"
+  },
+  {
+    "revision": "faf8f7dabdbe581ef275",
+    "url": "/simongame/js/chunk-vendors.e8e0b2a3.js"
+  },
+  {
+    "revision": "f5242e47bd3ac60fffd281cdeed08834",
+    "url": "/simongame/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/simongame/robots.txt"
+  }
+]);
